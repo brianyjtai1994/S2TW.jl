45abab1bc4a305d3616df1ddd7b7c2ccd658ca54
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-08-07 05:27:44 +0800

    Init the S2TW operation
    1. struct S2TW for configuration
    2. struct Lexer for converting

---
+++ src/S2TW.jl
@@ -0,0 +1,81 @@
+module S2TWConverter
+
+using DelimitedFiles
+
+export S2TW, s2tw
+
+abstract type AbstractS2TW end
+
+include("lexer.jl")
+
+struct S2TW{S} <: AbstractS2TW dep::Vector{Dict{S, S}} end
+
+function S2TW()
+    字典 = Dict{String, String}()
+
+    @inbounds for deps_path in ("src/deps/字典/", "src/deps/異體/")
+        for filename in readdir(deps_path)
+            temp_arr = readdlm(deps_path * filename, ',', String, comments=true, comment_char='#')
+
+            @simd for i in axes(temp_arr, 1)
+                字典[temp_arr[i, 1]] = temp_arr[i, 2]
+            end
+        end
+    end
+
+    二字 = Dict{String, String}()
+    temp_arr = readdlm("src/deps/用語/二字.txt", ',', String, comments=true, comment_char='#')
+    @inbounds @simd for i in axes(temp_arr, 1)
+        二字[temp_arr[i, 1]] = temp_arr[i, 2]
+    end
+
+    三字 = Dict{String, String}()
+    temp_arr = readdlm("src/deps/用語/三字.txt", ',', String, comments=true, comment_char='#')
+    @inbounds @simd for i in axes(temp_arr, 1)
+        三字[temp_arr[i, 1]] = temp_arr[i, 2]
+    end
+
+    return S2TW(Dict{String, String}[字典, 二字, 三字])
+end
+
+s2tw(str::S) where S<:AbstractString = s2tw(Lexer(str), S2TW())
+
+function s2tw(lx::Lexer, s2tw::S2TW)
+    ret = ""
+
+    while peek_char(lx) != EOFChar
+        ret *= accept_char!(lx, s2tw)
+    end
+
+    return ret
+end
+
+# check if next char is one of among the valid ones
+function accept_char!(lx::Lexer, s2tw::S2TW)
+    rdx       = 3
+    trial     = peek_char(lx, rdx)
+    not_found = !haskey(s2tw.dep[rdx], trial)
+
+    rdx -= 1
+
+    while not_found && rdx > 0
+        trial     = peek_char(lx, rdx)
+        not_found = !haskey(s2tw.dep[rdx], trial)
+
+        rdx -= 1
+    end
+
+    next_char!(lx, rdx+1)
+
+    return s2tw_convert(trial, s2tw, rdx+1)
+end
+
+function s2tw_convert(str::S, s2tw::S2TW, rdx::Int) where S<:AbstractString
+    if haskey(s2tw.dep[rdx], str)
+        return s2tw.dep[rdx][str]
+    else
+        return str
+    end
+end
+
+end
---
+++ src/lexer.jl
@@ -0,0 +1,79 @@
+mutable struct Lexer{S, U} <: AbstractS2TW
+    str::S # string being scanned
+    idx::U # start position of this item (lexeme)
+    pos::U # current position in the scanned string (start from 0)
+end
+
+# @code_warntype
+Lexer(str::S) where S<:AbstractString = Lexer(str, 1, 0)
+
+Base.show(io::IO, lx::Lexer) = print(io, string("Lexer(", lx.idx, ", ", lx.pos, ")"))
+
+const EOFChar = Char(0xA1)         # End Of File Char
+const EOFStr  = string(Char(0xA1)) # End Of File String
+
+# to see what is the current char, @code_warntype
+function this_char(lx::Lexer) # -> current_char
+    if lx.pos < 1
+        error("Can't ask for current Char before 1st Char has been fetched")
+
+    elseif lx.pos > lastindex(lx.str)
+        return EOFStr
+    end
+
+    return lx.str[lx.pos]
+end
+
+# to see what is the next char, @code_warntype
+function peek_char(lx::Lexer)
+    pos = nextind(lx.str, lx.pos)
+
+    if pos > lastindex(lx.str)
+        return EOFChar
+    end
+
+    return lx.str[pos]
+end
+
+# to see what the next phrase is (with length of `rdx + 1`), @code_warntype
+function peek_char(lx::Lexer, rdx::Int)
+    pos = lx.pos
+
+    while rdx > 0
+        pos = nextind(lx.str, pos)
+
+        if pos > lastindex(lx.str)
+            return EOFStr
+        end
+
+        rdx -= 1
+    end
+
+    return lx.str[lx.idx:pos]
+end
+
+# update Lexer to next char, @code_warntype
+function next_char!(lx::Lexer)
+    lx.pos = lx.idx
+    lx.idx = nextind(lx.str, lx.pos)
+end
+
+function next_char!(lx::Lexer, rdx::Int)
+    while rdx > 0
+        next_char!(lx); rdx -= 1
+    end
+end
+
+# update Lexer to previous char
+function prev_char!(lx::Lexer) # -> backup_char
+    lx.pos = prevind(lx.str, lx.pos)
+
+    return lx.str[lx.pos]
+end
+
+# skip the space-like char, @code_warntype
+function skip_char!(lx::Lexer)
+    while isspace(peek_char(lx))
+        next_char!(lx)
+    end
+end
