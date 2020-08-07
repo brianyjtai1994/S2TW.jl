c2b097f8dd02cb95aca5d04a965db3c2eea9589c
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-08-08 01:39:17 +0800

    Toward Julia Pkg.

--- src/S2TwTrans.jl
+++ src/S2TwTrans.jl
@@ -1,16 +1,16 @@
-module S2TWConverter
+module S2TwTrans
 
 using DelimitedFiles
 
-export S2TW, s2tw
+export S2TWConfig, s2tw
 
 abstract type AbstractS2TW end
 
 include("lexer.jl")
 
-struct S2TW{S} <: AbstractS2TW dep::Vector{Dict{S, S}} end
+struct S2TWConfig{S} <: AbstractS2TW dep::Vector{Dict{S, S}} end
 
-function S2TW()
+function S2TWConfig()
     字典 = Dict{String, String}()
 
     @inbounds for deps_path in ("src/deps/字典/", "src/deps/異體/")
@@ -41,12 +41,12 @@ function S2TW()
         四字[temp_arr[i, 1]] = temp_arr[i, 2]
     end
 
-    return S2TW(Dict{String, String}[字典, 二字, 三字, 四字])
+    return S2TWConfig(Dict{String, String}[字典, 二字, 三字, 四字])
 end
 
-s2tw(str::S) where S<:AbstractString = s2tw(Lexer(str), S2TW())
+s2tw(str::S) where S<:AbstractString = s2tw(Lexer(str), S2TWConfig())
 
-function s2tw(lx::Lexer, s2tw::S2TW)
+function s2tw(lx::Lexer, s2tw::S2TWConfig)
     ret = ""
 
     while peek_char(lx) != EOFChar
@@ -58,7 +58,7 @@ function s2tw(lx::Lexer, s2tw::S2TW)
 end
 
 # check if next char is one of among the valid ones
-function accept_char!(lx::Lexer, s2tw::S2TW)
+function accept_char!(lx::Lexer, s2tw::S2TWConfig)
     rdx       = 4
     trial     = peek_char(lx, rdx)
     not_found = !haskey(s2tw.dep[rdx], trial)
@@ -77,7 +77,7 @@ function accept_char!(lx::Lexer, s2tw::S2TW)
     return s2tw_convert(trial, s2tw, rdx+1)
 end
 
-function s2tw_convert(str::S, s2tw::S2TW, rdx::Int) where S<:AbstractString
+function s2tw_convert(str::S, s2tw::S2TWConfig, rdx::Int) where S<:AbstractString
     if haskey(s2tw.dep[rdx], str)
         return s2tw.dep[rdx][str]
     else
